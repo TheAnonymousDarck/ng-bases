1b6cd81ce1b3f39eb8ea7309a12881e5504f2541
Enrique Resendiz <k.ventus250@gmail.com>
2023-12-05 07:00:09 -0600

    feat: Angular Course -  Section 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'ng-bases';
+  public title: string = 'Mi primera App';
+  // public counter: number = 10;
+
+  // increaseBy(value: number): void {
+  //   this.counter += value;
+  // }
+  // decreaseBy(value: number): void {
+  //   this.counter -= value;
+  // }
+
+  // resetCounter(): void {
+  //   this.counter = 10;
+  // }
 }
